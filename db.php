f61cf828a29f7fac6fd8dd3ca0f188808a6c865f
Deepakm8-b <deepakbhandari982@gmail.com>
2026-07-23 19:46:39 +1000

    Use Railway native MySQL

--- db.php
+++ db.php
@@ -1,19 +1,18 @@
 <?php
-define('DB_HOST', 'sakura.proxy.rlwy.net');
-define('DB_NAME', 'railway');
-define('DB_USER', 'root');
-define('DB_PASS', 'vQUSlGXnLhUIkdAOPMuehREotZMfDwqd');
-define('DB_PORT', 43594);
+$host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
+$port = getenv('MYSQLPORT') ?: 3306;
+$db = getenv('MYSQLDATABASE') ?: 'railway';
+$user = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: '';
 
 try {
     $pdo = new PDO(
-        'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4',
-        DB_USER,
-        DB_PASS,
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $password,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]
     );
 } catch (PDOException $e) {
