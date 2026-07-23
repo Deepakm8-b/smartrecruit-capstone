06186f94fecdc102829a3625ec0d9528da29c5fb
Deepakm8-b <deepakbhandari982@gmail.com>
2026-07-23 20:04:19 +1000

    Connect to PlanetScale MySQL

--- db.php
+++ db.php
@@ -1,15 +1,9 @@
 <?php
-$host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
-$port = getenv('MYSQLPORT') ?: 3306;
-$db = getenv('MYSQLDATABASE') ?: 'railway';
-$user = getenv('MYSQLUSER') ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: '';
-
 try {
     $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $password,
+        'mysql:host=aws.connect.psdb.cloud;dbname=smartrecruit;charset=utf8mb4',
+        'ud613zr2bhkrz75e1ifc',
+        'pscale_pw_YFhN8CyGU9t6sRtPAiMuVlP76rIb50T5Y8VAiuLyLT',
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
